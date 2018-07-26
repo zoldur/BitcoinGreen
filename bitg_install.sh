bdd11982c8e9570b16ad84b6c7e9c6dfd5d0abee
Zoldur <zoldur99@gmail.com>
2018-07-26 19:57:45 +0100

    Add nodes

--- bitg_install.sh
+++ bitg_install.sh
@@ -123,6 +123,22 @@ maxconnections=256
 masternode=1
 externalip=$NODEIP:$COIN_PORT
 masternodeprivkey=$COINKEY
+
+addnode=51.15.198.252
+addnode=51.15.206.123
+addnode=51.15.66.234
+addnode=51.15.86.224
+addnode=51.15.89.27
+addnode=51.15.57.193
+addnode=134.255.232.212
+addnode=185.239.238.237
+addnode=185.239.238.240
+addnode=134.255.232.212
+addnode=207.148.26.77
+addnode=207.148.19.239
+addnode=108.61.103.123
+addnode=185.239.238.89
+addnode=185.239.238.92
 EOF
 }
 
